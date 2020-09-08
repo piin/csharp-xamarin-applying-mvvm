246fad26b0e414f15be56b47ee5bf79c96d08068
jair.pinedo <jair.pinedo@unosquare.com>
2020-09-08 02:18:49 -0500

    Remove useless code

--- Roster.Client/ViewModels/HomeViewModel.cs
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -8,58 +8,27 @@ namespace Roster.Client.ViewModels
 {
     public class HomeViewModel : INotifyPropertyChanged
     {
-        #region Public properties
+        private string _title = "Roster App";
 
         public event PropertyChangedEventHandler PropertyChanged;
-        public ICommand UpdateApplicationCommand { get; set; }
+        public Command UpdateApplicationCommand { get;}
         public string Title {
-            get
-            {
-                return _title;
-            }
+            get => _title;
             set
             {
-                if (value != _title)
-                {
-                    _title = value;
-                    NotifyPropertyChanged();
-                }                
+                _title = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Title)));
             }
         }
-
-        #endregion
-
-        #region Private properties
-
-        private string _title;
-
-        #endregion
-
-        #region Public methods
 
         public HomeViewModel()
         {
-            UpdateApplicationCommand = new Command(OnUpdateApplicationCommand);
+            UpdateApplicationCommand = new Command(UpdateApplicationCommandExecute);
         }
-        #endregion
 
-        #region Private methods
-
-        private void OnUpdateApplicationCommand()
+        private void UpdateApplicationCommandExecute()
         {
             Title = "Roster App (v2.0)";
         }
-
-        // This method is called by the Set accessor of each property.
-        // The CallerMemberName attribute that is applied to the optional propertyName
-        // parameter causes the property name of the caller to be substituted as an argument.
-        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-        #endregion
     }
 }
