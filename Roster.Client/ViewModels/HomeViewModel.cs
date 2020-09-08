832da65877fc015fb563deb8a54243574b28ba8e
jair.pinedo <jair.pinedo@unosquare.com>
2020-09-07 22:03:41 -0500

    Add HomeViewModel

---
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace Roster.Client.ViewModels
+{
+    public class HomeViewModel : INotifyPropertyChanged
+    {
+        #region Public properties
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Command UpdateApplicationCommand { get; set; }
+        public string Title { get; set; } = "Roster App";
+        #endregion
+
+        public HomeViewModel()
+        {
+        }
+
+        #region Private methos
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
